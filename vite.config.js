1a96d7a0d174e472eff697b2faa724b03d31051f
Michael Becker <21beckem8@gmail.com>
2026-02-12 16:40:18 -0700

    fixed building for multiple pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
@@ -9,5 +10,11 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html')
+      }
+    }
   },
 });
