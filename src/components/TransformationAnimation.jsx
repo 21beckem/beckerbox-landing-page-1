1ae558590bf1c5d82493c51a56d37d58707254f2
Michael Becker <21beckem8@gmail.com>
2026-02-25 16:43:36 -0700

    added phone

--- src/components/TransformationAnimation.jsx
+++ src/components/TransformationAnimation.jsx
@@ -6,7 +6,7 @@ const TransformationAnimation = (props) => {
   return (
     <>
       <div class="animation-container">
-        <div class="remote">
+        <div class="object remote" style="display: none;">
             <div class="side front"></div>
             <div class="side back"></div>
             <div class="side top"></div>
@@ -16,7 +16,7 @@ const TransformationAnimation = (props) => {
             <div class="extra remote-button1"></div>
             <div class="extra remote-button2"></div>
         </div>
-        <div class="console">
+        <div class="object console" style="display: none;">
             <div class="side front"></div>
             <div class="side back"></div>
             <div class="side top"></div>
@@ -24,6 +24,33 @@ const TransformationAnimation = (props) => {
             <div class="side left"></div>
             <div class="side right"></div>
             <div class="extra disc-slot"></div>
+        </div>
+        <div class="object phone" style="display: block;">
+            <div class="side front"></div>
+            <div class="side back"></div>
+            <div class="side top"></div>
+            <div class="side bottom"></div>
+            <div class="side left"></div>
+            <div class="side right"></div>
+            <div class="extra screen"></div>
+        </div>
+        <div class="object laptop" style="display: none;">
+            <div class="keyboard">
+              <div class="side front"></div>
+              <div class="side back"></div>
+              <div class="side top"></div>
+              <div class="side bottom"></div>
+              <div class="side left"></div>
+              <div class="side right"></div>
+            </div>
+            <div class="screen">
+              <div class="side front"></div>
+              <div class="side back"></div>
+              <div class="side top"></div>
+              <div class="side bottom"></div>
+              <div class="side left"></div>
+              <div class="side right"></div>
+            </div>
         </div>
       </div>
     </>
