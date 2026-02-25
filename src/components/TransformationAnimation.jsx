a9b5b79e09f214bda6e4d757b4dc322f46e6da5d
Michael Becker <21beckem8@gmail.com>
2026-02-25 16:21:12 -0700

    added console disc slot and remote button

--- src/components/TransformationAnimation.jsx
+++ src/components/TransformationAnimation.jsx
@@ -7,20 +7,23 @@ const TransformationAnimation = (props) => {
     <>
       <div class="animation-container">
         <div class="remote">
-            <div class="front"></div>
-            <div class="back"></div>
-            <div class="top"></div>
-            <div class="bottom"></div>
-            <div class="left"></div>
-            <div class="right"></div>
+            <div class="side front"></div>
+            <div class="side back"></div>
+            <div class="side top"></div>
+            <div class="side bottom"></div>
+            <div class="side left"></div>
+            <div class="side right"></div>
+            <div class="extra remote-button1"></div>
+            <div class="extra remote-button2"></div>
         </div>
         <div class="console">
-            <div class="front"></div>
-            <div class="back"></div>
-            <div class="top"></div>
-            <div class="bottom"></div>
-            <div class="left"></div>
-            <div class="right"></div>
+            <div class="side front"></div>
+            <div class="side back"></div>
+            <div class="side top"></div>
+            <div class="side bottom"></div>
+            <div class="side left"></div>
+            <div class="side right"></div>
+            <div class="extra disc-slot"></div>
         </div>
       </div>
     </>
