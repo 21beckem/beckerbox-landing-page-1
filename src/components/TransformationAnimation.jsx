150efdfb61ef7bbe7f23d86018fc8e96c56a048a
Michael Becker <21beckem8@gmail.com>
2026-02-25 17:17:27 -0700

    laptop rotates around right hinge now!

--- src/components/TransformationAnimation.jsx
+++ src/components/TransformationAnimation.jsx
@@ -6,7 +6,7 @@ const TransformationAnimation = (props) => {
   return (
     <>
       <div class="animation-container">
-        <div class="object remote" style="display: none;">
+        <div class="object remote" style="display: block;">
             <div class="side front"></div>
             <div class="side back"></div>
             <div class="side top"></div>
@@ -16,7 +16,7 @@ const TransformationAnimation = (props) => {
             <div class="extra remote-button1"></div>
             <div class="extra remote-button2"></div>
         </div>
-        <div class="object console" style="display: none;">
+        <div class="object console" style="display: block;">
             <div class="side front"></div>
             <div class="side back"></div>
             <div class="side top"></div>
@@ -25,7 +25,7 @@ const TransformationAnimation = (props) => {
             <div class="side right"></div>
             <div class="extra disc-slot"></div>
         </div>
-        <div class="object phone" style="display: none;">
+        <div class="object phone" style="display: block;">
             <div class="side front"></div>
             <div class="side back"></div>
             <div class="side top"></div>
