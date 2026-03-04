9d1dabe8ea4bcd7937b1483fbccfa8de082b8212
Michael Becker <21beckem8@gmail.com>
2026-03-04 12:15:24 -0700

    added privacy policy to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,8 @@ const Footer = () => {
         <center>
           {/* <p>BeckerBox helps you connect the devices you already own for motion-first play on PC.</p> */}
           <p>&copy; 2026 BeckerSuite LLC. All rights reserved.</p>
+          <br />
+          <a href="https://beckersuite.com/privacy/" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
         </center>
       </div>
     </footer>
